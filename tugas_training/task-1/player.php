fd8859de938f3392afb12de80cc8166f2e6335cc
Ade Bayu Martin <adebayu39@gmail.com>
2016-08-10 16:48:43 +0700

    Installing laravel end edit player.php

--- tugas_training/task-1/player.php
+++ tugas_training/task-1/player.php
@@ -1,88 +1,85 @@
 <?php
+  class Player {
+  private $name, $blood, $mana;
 
-class Player {
-  public $name;
-  public $blood = 100;
-  public $manna = 40;
-
-  function index(){
-    echo '
-    # ============================== #</br>
-    # Welcome to the Battle Arena #</br>
-    # ------------------------------------------------- ---- #</br>
-    # Description: #</br>
-    # 1 type "new" to create a character #</br>
-    # 2. type "start" to begin the fight #</br>
-    # ------------------------------------------------- ---- #</br>
-    # Current Player: # ';
-    # - #
-    echo '# * Max player 2 or 3 #</br>
-    # ------------------------------------------------- ---- #</br>
-    ';
+  function __construct($name){
+    $this->name = $name;
+    $this->blood = 100;
+    $this->mana = 40;
   }
 
-  public function new_player($name_input){
-    $this->name = $name_input;
-  }
-
-
-
-  static function start($start_input){
-    echo '
-    # ============================== #</br>
-    # Welcome to the Battle Arena #</br>
-    # ------------------------------------------------- ---- #</br>
-    # Battle Start #';
-     echo '
-     who will attack =';
-     echo $name;
-     echo '
-     who attacked';
-     echo $name;
-     
-  }
-
-  public function attack(){
-    $this->manna -= 10;
-    Player::result;
+ public function attack(){
+    $this->manna = $this->mana - 10;
   }
 
   public function defend(){
-    $this->blood -= 20;
-    $this->manna += 10;
-    Player::result;
-  }
-
-  static function result(){
-    echo '
-    # ============================== #</br>
-    # Welcome to the Battle Arena #</br>
-    # ------------------------------------------------- ---- #</br>
-    # Battle Start # </br>';
-     echo 'who will attack =</br>';
-     echo $this->name;
-     echo 'who attacked';
-     echo $this->name;
-     echo 'Description=';
-     echo $this->name, '==== manna = ';
-     echo $this->manna, ', blood = ';
-     echo $this->blood;
-  }
-
-  function end(){
-    echo '
-    # Battle Royal #</br>
-    # Game Over #
-    ';
+    $this->blood = $this->blood - 25;
   }
 
 }
 
-$player = new Player;
-echo $player->index();
 
-while ($player->blood = 0){
-  echo $player->end();
+  class Menu{
+      private $players = [];
+      
+      public function main() {
+        echo '# ============================== #
+        # Welcome to the Battle Arena #
+        # ------------------------------------------------- ---- #
+        # Description: #
+        # 1 type "new" to create a character #
+        # 2. type "start" to begin the fight #
+        # ------------------------------------------------- ---- #
+        # * Max player 2 or 3 #
+        # ------------------------------------------------- ---- #
+        # Current Player: #
+        ';
+        fscanf(STDIN, "%s\n", $input);
+        switch ($input) {
+           case "new": echo $this->add_player();
+             break;
+           case "start": echo $this->start();
+             break;
+           default: echo "wrong input!!!"; echo $this->main();
+         } //(strcmp($input, "new" )) {
+//            echo $this->add_player();
+  //        } elseif (strcmp($input, "start" )){
+    //        echo $this->start();
+      //    } else {
+        //    echo "wrong input !!!!";
+          //  echo $this->main();
+            //}
+      }
+
+
+
+      function add_player() {
+        echo "type your name here : ";
+        fscanf(STDIN, "%s\n" , $name);
+        $this->players[$name] = new Player($name);
+        $this->main();
+      }
+
+      function start(){
+        if ($this->player == null) {
+          echo "Pemain Tidak Ada!!! Silahkan Buat Pemain Baru!!!";
+          echo $this->add_player();
+        } else {
+        echo '
+        # ============================== #</br>
+        # Welcome to the Battle Arena #</br>
+        # ------------------------------------------------- ---- #</br>
+        # Battle Start #';
+        echo '
+        who will attack =';
+        return $this->player;
+        echo '
+        who attacked';
+        return $this->player;
+      }
+    }
 } 
 
+  $game = new Menu;
+  echo $game->main();
 ?>
